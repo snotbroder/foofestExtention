f1640ba95c39b0da48fb5e5641abc76a8bcb0a38
Joris <jote0002@stud.kea.dk>
2025-01-07 10:24:57 +0100

    tilføjet shadcn. tilføjet søgefunktion. begyndt at style yderligere

--- src/app/components/LineUpComponent.jsx
+++ src/app/components/LineUpComponent.jsx
@@ -5,6 +5,7 @@ import useSWR from "swr";
 import BandCard from "./BandCard";
 import { IoMdArrowDropdown } from "react-icons/io";
 import { api } from "../api";
+import { Input } from "@/components/ui/input";
 
 const fetcher = (url) => fetch(url).then((res) => res.json());
 
@@ -16,6 +17,8 @@ function LineUpComponent() {
   let [pickedGenre, setPickedGenre] = useState("");
 
   let { data, error, isLoading } = useSWR(BANDS_URL, fetcher);
+  const [search, setSearch] = useState("");
+  console.log(search);
 
   useEffect(() => {
     if (!isLoading) {
@@ -46,34 +49,40 @@ function LineUpComponent() {
 
   return (
     <article>
-      <div className="font-bold justify-center flex-row  text-2xl sm:text-3xl inline-flex gap-8 font-rethink text-main-1 my-6">
-        <div className="select-container ">
-          <select className="select-box" name="genres" id="select-genre" onChange={(e) => setPickedGenre(e.target.value)}>
-            <option value="">Show all results</option>
-            {genres.map((genre, index) => {
-              return (
-                <option value={genre} key={index}>
-                  {genre}
-                </option>
-              );
-            })}
-          </select>
-          <div className="select-icon cursor-pointer">
-            <IoMdArrowDropdown size={35} />
+      <section className="border-main-1 border-solid border-2 p-6 rounded-rounded-reg">
+        <div className="flex justify-between gap-16 mb-4">
+          <div className="flex flex-col">
+            <label className="text-xl sm:text-xl font-rethink text-main-1" htmlFor="select-genre">
+              Sort by genre
+            </label>
+            <select className="bg-accent-1 text-xl sm:text-md font-rethink text-main-1" name="genres" id="select-genre" onChange={(e) => setPickedGenre(e.target.value)}>
+              <option value="">Show all results</option>
+              {genres.map((genre, index) => {
+                return (
+                  <option value={genre} key={index}>
+                    {genre}
+                  </option>
+                );
+              })}
+            </select>
           </div>
-        </div>
-        <div>
-          <button className="text-main-1 uppercase font-rethink " onClick={handleAscending}>
+
+          <button className="text-main-1 bg-accent-1 text-xl sm:text-md font-rethink" onClick={handleAscending}>
             {isListAscending ? "Z-A" : "A-Z"}
           </button>
         </div>
-      </div>
+        <Input onChange={(e) => setSearch(e.target.value)} type="bandsearch" placeholder="search" />
+      </section>
       <div className="bg-[url('/illustrations/svg/circle.svg')] bg-contain bg-repeat-y bg-no-repeat-x  -mx-mobile lg:-mx-desktop py-16">
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 items-center place-items-center gap-10 mx-mobile pb-16 lg:mx-desktop">
-          {currentData.map((band) => {
-            const logo = band.logo.startsWith("http") ? band.logo : `${BASE_URL}${band.logo}`;
-            return (!pickedGenre || band.genre === pickedGenre) && <BandCard alt={`Logo of ${band.name}`} key={band.name} imgSrc={logo} name={band.name} genre={band.genre} slug={band.slug} />;
-          })}
+          {currentData
+            .filter((band) => {
+              return search.toLowerCase() === "" ? band : band.name.toLowerCase().includes(search);
+            })
+            .map((band) => {
+              const logo = band.logo.startsWith("http") ? band.logo : `${BASE_URL}${band.logo}`;
+              return (!pickedGenre || band.genre === pickedGenre) && <BandCard alt={`Logo of ${band.name}`} key={band.name} imgSrc={logo} name={band.name} genre={band.genre} slug={band.slug} />;
+            })}
         </div>
       </div>
     </article>
